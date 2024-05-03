21b83f8c6835bd1935ff78c816c6107e6cb4bdb9
Esther Dennis <148415275+estherszliu@users.noreply.github.com>
2024-05-04 09:49:38 +1000

    feat: class basic functionality and inheritance

---
+++ Book.js
@@ -0,0 +1,14 @@
+
+
+class Book extends Media{
+    constructor(newName, newEdition ="first"){
+        super(newName);
+
+        this.edition = newEdition;
+    }
+}
+
+module.exports = {
+    Book
+}
+
